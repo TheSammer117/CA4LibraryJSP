9e73c5e2673139fe3723ec879182fcfd01a43a55
emmahyy <emmahyy@users.noreply.github.com>
2018-12-08 07:23:55 +0000

    add returnbook command

--- src/java/Commands/BorrowBookCommand.java
+++ src/java/Commands/BorrowBookCommand.java
@@ -17,8 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- *
- * @author omy
+ * Team: Hernel Provido, Sami Mahmoud, Haiyun Yu
+ * @author Haiyun Yu d00188956 
  */
 public class BorrowBookCommand implements Command {
 
